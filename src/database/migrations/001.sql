02c7a176680e568df30294dfafd427a6a621deef
Henry Pick <hpick@hmc.edu>
2023-05-16 10:14:18 -0700

    database containerization

---
+++ src/database/migrations/001.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS times(
+    id INT NOT NULL PRIMARY KEY,
+    athlete_id INT NOT NULL,
+    race_id INT NOT NULL,
+    time INT NOT NULL,
+    place INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS athletes(
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    year INT NOT NULL,
+    school_id INT NOT NULL,
+    sex INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS schools(
+    id INT NOT NULL PRIMARY KEY,
+    division INT NOT NULL,
+    name VARCHAR NOT NULL,
+    conference VARCHAR 
+);
+
+CREATE TABLE IF NOT EXISTS races(
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    date DATE NOT NULL,
+    meet_id INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS meets(
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    date DATE NOT NULL
+);
